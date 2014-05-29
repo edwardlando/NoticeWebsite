73d583a0691ec791ca0bc44814a9b13d52fb28cc
Yagil <yagilbu@gmail.com>
2014-05-28 21:44:46 -0700

    changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,8 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// 	alert("hello");
+	// alert("hello");
+	$("#phoneNumberInput").keypress(function() {
+		alert("hi");
+	});
 
-// });
+});
